0e70b491661a3da876cb193802f287e6b954d01c
Absurd <michaelbasalone@icloud.com>
2025-04-18 00:30:43 -0700

    added map component

---
+++ src/components/Map.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import '../App.css'
+
+function Map() {
+    const mapRef = useRef();
+    const mapContainerRef = useRef();
+
+    useEffect(() => {
+        mapboxgl.accessToken = "pk.eyJ1IjoibWljaGFlbC0tIiwiYSI6ImNtOWtzZjBnODB0NDUya3Byb3AyY2NtMncifQ.HFQAbCOMSzGiYDZzTdwCrQ";
+        mapRef.current = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            style: "mapbox://styles/michael--/cm9kutbdp00ot01rcecqthand/draft"
+        });
+
+        return () => {
+            mapRef.current.remove();
+        }
+    }, []);
+
+    return (
+        <>
+            <div id="map-container" ref={mapContainerRef} />
+        </>
+    )
+}
+
+export default Map;
